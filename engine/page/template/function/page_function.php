3163315f5ec64656dd519c23222ae3e5f446c873
xcaww <xcore@recoders.org>
2011-03-15 13:39:53 +1000

    added page template and structure

---
+++ engine/page/template/function/page_function.php
@@ -0,0 +1,18 @@
+<?php
+
+/*
+	@ OSKARR (beta)
+	@
+	@ ||...............||	
+	@    page_function
+	@ ||...............||
+	@
+	@ ||...............................................................||
+	@   The functions described here and other scripts in the directory
+	@   (if necessary) are used by the producers, acting as a backend
+	@   for the generation of the page
+	@ ||...............................................................||
+	
+*/
+
+?>
---
+++ engine/page/template/producer/page_producer.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+	@ OSKARR (beta)
+	@
+	@ ||...............||	
+	@    page_producer 
+	@ ||...............||
+	@
+	@ ||...............................................................||
+	@   This script utilizes the variables given from page_controller
+	@   and the associated functions based in the functions directory.
+	@ ||...............................................................||
+	
+*/
+
+?>
