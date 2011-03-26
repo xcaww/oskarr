7b4708a11f91ab3459a3331e5ebb28bba06caa1c
xcaww <xcore@recoders.org>
2011-03-26 12:11:20 +1000

    added htaccess + index implementation

--- index.php
+++ index.php
@@ -1,100 +1,99 @@
 <?php
 /*
-	@ OSKARR (beta)
-	@ Developed by xCore
-	@ Current Stage: in beginning of core development
-	@ Version: none...
-	
-	
-	# Instructions
-	
-	# Operation of the index is only available through url variables.
-	# Example: index.php?post=1
-	# Example: index.php?channel=rage
-	# Current structure and functionality (including minimum security) is very unstable...
-	
-	# Without a connected mySQL database, this will not function.
-	# Details on the database structure are yet to be released...
+
+ * OSKARR (beta)
+ * Developed by xCore
+ * Current Stage: in beginning of core development
+ * Version: none...
+
+ * Instructions
+
+ * Operation of the index is only available through url variables.
+ * Example: index.php?post=1
+ * Example: index.php?channel=rage
+ * Current structure and functionality (including minimum security) is very unstable...
+
+ * Without a connected mySQL database, this will not function.
+ * Details on the database structure are yet to be released...
 	
 */
 
-
-//load core function library + database class
-require("./engine/core.php");
-require("./engine/database.php");
+//load core function library + settings + database class
+require_once("./engine/core.php");
+require_once("./engine/settings.php");
+require_once("./engine/database.php");
 
 //Let's roll...
 $core = new core();
-$module['moduleName'] = "pagebits"; 
-$module['execution_start'] = $core->execution_time(); 
-$module['moduleQuery'] = "header";
-
-$header = $core->call_module($module);  
-echo $header['header'];	
-
+$execution_start = $core->execution_time();
 $core->database_connect();
+$header = $core->call_module("pagebits", "header");
+echo $header['header'];
 
-//GET i VAR
-if(isset($_GET['i'])){
+    //GET i VAR
+    if(isset($_GET['i'])){
 
-	if(ctype_digit($_GET['i'])){
-	
-		$URL_i = (string) $_GET['i'];
-		
-	}else{
-	
-		$URL_i = "0";
-		
-	}
-	
-}else{
+	    if(ctype_digit($_GET['i'])){
 
-	$URL_i = "0";
-	
-}
+		    $URL_i = (string) $_GET['i'];
 
-//GET page VAR
-if(isset($_GET['page'])){
+	    }else{
 
-	if(ctype_alnum($_GET['page'])){
+		    $URL_i = "0";
 
-		$URL_page = (string) $_GET['page'];
-		$core->generate_page($URL_page, $URL_i);
-		
-	}
-//GET post VAR	
-}elseif(isset($_GET['post'])){
+	    }
 
-	if(ctype_alnum($_GET['post'])){
+    }else{
 
-		$URL_post = (string) $_GET['post'];
-		$core->generate_page("post", $URL_post);
-		
-	}
-//GET channel VAR	
-}elseif(isset($_GET['channel'])){
+	    $URL_i = "0";
 
-	if(ctype_alnum($_GET['channel'])){
+    }
 
-		$URL_channel = (string) $_GET['channel'];
-		$core->generate_page("channel", $URL_channel, $URL_i);
-		
-	}
-//GET handle VAR	
-}elseif(isset($_GET['handle'])){
+    //GET page VAR
+    if(isset($_GET['page'])){
 
-	if(ctype_alnum($_GET['handle'])){
+	    if(ctype_alnum($_GET['page'])){//TODO add unknown page error handler
 
-		$URL_handle = (string) $_GET['handle'];
-		//Needs to be written...
-		
-	}
-	
-}
+		    $URL_page = (string) $_GET['page'];
+		    $core->generate_page($URL_page, $URL_i);
+
+	    }
+    //GET post VAR
+    }elseif(isset($_GET['post'])){
+
+	    if(ctype_alnum($_GET['post'])){
+
+		    $URL_post = (string) $_GET['post'];
+		    $core->generate_page("post", $URL_post);
+
+	    }
+    //GET channel VAR
+    }elseif(isset($_GET['channel'])){
+
+	    if(ctype_alnum($_GET['channel'])){
+
+		    $URL_channel = (string) $_GET['channel'];
+		    $core->generate_page("channel", $URL_channel, $URL_i);
+
+	    }
+    //GET handle VAR
+    }elseif(isset($_GET['handle'])){
+
+	    if(ctype_alnum($_GET['handle'])){
+
+		    $URL_handle = (string) $_GET['handle'];
+		    //Needs to be written...
+
+	    }
+
+    }
 
 
-//end execution timer, close page tags
-$module['moduleQuery'] = "execution"; 
-$execution_time = $core->call_module($module);  
-echo $execution_time['execution'];	
+//execution time
+$execution_time = $core->call_module("pagebits", "execution", $execution_start);
+echo $execution_time['execution'];
+
+//close page
+$footer = $core->call_module("pagebits", "footer");
+echo $footer['footer'];
 ?>
